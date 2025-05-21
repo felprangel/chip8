39d755c6a5d397f577c93a8fd0802923f7d9002f
felprangel <feliperangel897@gmail.com>
2025-05-20 21:14:55 -0300

    adiciona função para inicializar o sdl

--- chip8.c
+++ chip8.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "SDL.h"
+
+bool init_sdl(void)
+{
+    if (SDL_init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
+        SDL_Log("Could not initialize SDL subsystems! %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
