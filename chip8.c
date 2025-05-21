3214ede7e80ddbf1bd9d284e7fc626a4b4097ec5
felprangel <feliperangel897@gmail.com>
2025-05-20 21:41:43 -0300

    adiciona struct de objeto sdl e adiciona parametros na funcao

--- chip8.c
+++ chip8.c
@@ -4,7 +4,11 @@
 
 #include "SDL.h"
 
-bool init_sdl(void)
+typedef struct {
+    SDL_Window *window;
+} sdl_object;
+
+bool init_sdl(sdl_object *sdl, const config_object config)
 {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
         SDL_Log("Could not initialize SDL subsystems! %s\n", SDL_GetError());
