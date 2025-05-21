dd56979ea6cd008079abfc20735589f2d1d4450f
felprangel <feliperangel897@gmail.com>
2025-05-20 21:19:04 -0300

    corrige typo

--- chip8.c
+++ chip8.c
@@ -6,7 +6,7 @@
 
 bool init_sdl(void)
 {
-    if (SDL_init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
         SDL_Log("Could not initialize SDL subsystems! %s\n", SDL_GetError());
         return false;
     }
